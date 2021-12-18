fe4ceaab1f0da8c332ce22910a36aac0367fc8b5
Sanches23rx <rudzsan@gmail.com>
2021-12-19 00:20:10 +0300

    commit1

--- main.go
+++ main.go
@@ -4,43 +4,54 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 type Message struct {
-	UserName    string
-	MessageText string
-	TimeStamp   string
+	Text string `json:"text"`
 }
 
 func main() {
-	r := chi.NewRouter()
-	fmt.Print("Service is running")
 	messages := make([]Message, 0)
 
-	r.Post("/api/Messenger", func(w http.ResponseWriter, r *http.Request) {
-		mes := Message{}
-		raw, _ := ioutil.ReadAll(r.Body)
-		json.Unmarshal(raw, &mes)
-		fmt.Println(mes)
-		messages = append(messages, mes)
+	r := chi.NewRouter()
 
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://*"},
+		AllowedMethods: []string{"GET", "POST"},
+	}))
+
+	r.Get("/api/echo", func(w http.ResponseWriter, r *http.Request) {
+		data, _ := ioutil.ReadAll(r.Body)
+		w.Write(data)
 	})
-
-	r.Get("/api/Messenger/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := chi.URLParam(r, "id")
-		id_num, _ := strconv.Atoi(id)
-		if id_num >= 0 && id_num < len(messages) {
-			msg := messages[id_num]
+	r.Post("/api/messages", func(w http.ResponseWriter, r *http.Request) {
+		data, _ := ioutil.ReadAll(r.Body)
+		fmt.Println("MSG SAVED:", string(data))
+		msg := Message{}
+		if err := json.Unmarshal(data, &msg); err != nil {
+			fmt.Errorf("can't unmarshal: %s", err)
+		}
+		messages = append(messages, msg)
+	})
+	r.Get("/api/messages/{id}", func(w http.ResponseWriter, r *http.Request) {
+		str_id := chi.URLParam(r, "id")
+		id, err := strconv.Atoi(str_id)
+		fmt.Println("MSG SENT:", str_id)
+		if err != nil {
+			log.Fatalf("can't parse id: %s", err)
+		}
+		if id >= 0 && id < len(messages) {
+			msg := messages[id]
 			data, _ := json.Marshal(msg)
 			w.Write(data)
 		}
 	})
 
-	http.ListenAndServe(
-		"5000",
-		r,
-	)
+	fmt.Println("Ready")
+	http.ListenAndServe(":5000", r)
 }
